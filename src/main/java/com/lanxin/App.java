a9c3fc32436c97fcfc37f9f545e2574185f6d99a
lznbnb <2547783110@qq.com>
2019-05-11 09:46:22 +0800

    test666

--- src/main/java/com/lanxin/App.java
+++ src/main/java/com/lanxin/App.java
@@ -11,7 +11,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class App {
     public static void main( String[] args ) {
-        System.out.println( "Hello World!sss2333" );
+        System.out.println( "Hello World!sss23334" );
         SpringApplication.run(App.class,args);
     }
 }
